60767ca5e389ae5a76e2668f2e73a987056dd1c5
Cortantse <xuzhj33@mail2.sysu.edu.cn>
2024-07-20 19:56:29 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 pub const SERVER_CAPACITY: usize = 65535;
 pub const SERVER_PORT: &str = "8080";
-pub const SERVER_IP: &str = "127.0.0.1";
+pub const SERVER_IP: &str = "172.29.58.50";
 
 pub const CLIENT_MAX_RETRIES: usize = 5;
 pub const CLIENT_MAX_RETRIES_PER_REQUEST: u64 = 1;
